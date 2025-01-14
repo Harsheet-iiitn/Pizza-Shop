ddbdbb7db5d977ee786e3ae75608350b0a2e705e
Harsheet Aran <bt21cse029@iiitn.ac.in>
2025-01-14 05:39:50 +0000

    committed changes

--- src/DataStorage.jsx
+++ src/DataStorage.jsx
@@ -19,6 +19,32 @@ function LEN() {
   return 10;
 }
 
+function showPlaced() {
+  return placed;
+}
+
+function showMaking() {
+  return making;
+}
+
+function showReady() {
+  return ready;
+}
+
+function showPicked() {
+  return picked;
+}
+
+function Change(a) {
+  if (placed.find(a) != undefined) {
+    making.push(a);
+  } else if (making.find(a) != undefined) {
+    ready.push(a);
+  } else if (ready.find(a) != undefined) {
+    picked.push(a);
+  }
+}
+
 export default DS;
 
-export { LEN };
+export { LEN, showPicked, showPlaced, showMaking, showReady, Change };
--- src/MainStageSection.jsx
+++ src/MainStageSection.jsx
@@ -1,18 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 
-function MS() {
-  return (
-    <div>
-      <table border="black">
-        <tr>
-          <th>Order Placed</th>
-          <th>Order In Making</th>
-          <th>Order Ready</th>
-          <th>Order Picked Up</th>
-        </tr>
-      </table>
-    </div>
-  );
-}
+function MainStage() {}
 
-export default MS;
+export default MainStage;
